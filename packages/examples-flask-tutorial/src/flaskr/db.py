8e884b864ac42de0d70302aed431e5c3432880cd
A.suzuki <suzu.devworks@gmail.com>
2025-11-29 15:32:20 +0000

    Fix spell-checker.

--- packages/examples-flask-tutorial/src/flaskr/db.py
+++ packages/examples-flask-tutorial/src/flaskr/db.py
@@ -8,7 +8,6 @@ from flask import Flask, current_app, g
 
 def get_db() -> sqlite3.Connection | Any:
     if "db" not in g:
-        # spell-checker:words PARSE_DECLTYPES
         g.db = sqlite3.connect(current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = sqlite3.Row
 
@@ -34,7 +33,6 @@ def init_db() -> None:
             script = data.decode("utf-8")
         else:
             script = str(data)
-        # spell-checker:words executescript
         db.executescript(script)
 
 
@@ -46,7 +44,6 @@ def init_db_command() -> None:
 
 
 def init_app(app: Flask) -> None:
-    # spell-checker:words teardown_appcontext
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
