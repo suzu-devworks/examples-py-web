suzu-devworks/examples-py

Add MJPEG streaming using OpenCV example to flask-media.

src/web/examples-flask-media/src/app.py
@@ -1,15 +1,19 @@
 from typing import Any, Mapping
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
+    from examples.webcam.mjpeg import blueprint as webcam_mjpeg
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    # a simple page that says hello
+    # add blueprints
+    app.register_blueprint(webcam_mjpeg, url_prefix="/webcam/mjpeg")
+
     @app.route("/")
-    def hello() -> str:
-        return "Hello, World!"
+    def index() -> str:
+        return render_template("index.html")
 
     return app

suzu-devworks/examples-py-web

packages/examples-flask-media/src/app.py
@@ -1,15 +1,19 @@
 from typing import Any, Mapping
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
+    from examples_flask_media.webcam.mjpeg import bp as bp_webcam_mjpeg
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    # a simple page that says hello
+    # add blueprints
+    app.register_blueprint(bp_webcam_mjpeg, url_prefix="/webcam/mjpeg")
+
     @app.route("/")
-    def hello() -> str:
-        return "Hello, World!"
+    def index() -> str:
+        return render_template("index.html")
 
     return app