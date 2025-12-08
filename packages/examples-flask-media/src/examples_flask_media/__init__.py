5a81563f3584de0b31bac1612f3bfb1734c01eec
A.suzuki <suzu.devworks@gmail.com>
2025-12-08 09:22:49 +0000

    Add segmented video example to flask-media.

--- packages/examples-flask-media/src/examples_flask_media/__init__.py
+++ packages/examples-flask-media/src/examples_flask_media/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 
 
 def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
+    from .blueprints.video.segments import blueprint as bp_video_segments
     from .blueprints.webcam.mjpeg import blueprint as bp_webcam_mjpeg
 
     basicConfig(level=DEBUG)
@@ -14,6 +15,7 @@ def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
 
     # add blueprints
     app.register_blueprint(bp_webcam_mjpeg, url_prefix="/webcam/mjpeg")
+    app.register_blueprint(bp_video_segments, url_prefix="/video/segments")
 
     @app.route("/")
     def index() -> str:
---
+++ packages/examples-flask-media/src/examples_flask_media/blueprints/video/segments/__route__.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+from flask import Blueprint, Response, current_app, render_template, request
+
+from .utility import format_range_header, parse_range_header
+from .video_splitter import get_chunk
+
+bp = Blueprint("video_segments", __name__, static_folder="static", template_folder="templates")
+bp.add_url_rule("/", endpoint="index")
+
+source_path = os.getenv("VIDEO_SRC", default=str(Path.cwd() / "input.mp4"))
+chunk_size = int(os.getenv("VIDEO_CHUNK_SIZE", str(65535 * 2)))
+
+
+@bp.route("/")
+def index() -> str:
+    logger = current_app.logger
+    logger.debug(f"VIDEO_SRC: {source_path}")
+    logger.debug(f"VIDEO_CHUNK_SIZE: {chunk_size}")
+
+    return render_template("video/segments/index.html")
+
+
+@bp.after_request
+def after_request(response: Response) -> Response:
+    response.headers.add("Accept-Ranges", "bytes")
+    return response
+
+
+@bp.route("/video_feed")
+def video_feed() -> Response:
+    logger = current_app.logger
+
+    range_header = request.headers.get("Range", None)
+    logger.debug(f"chunks - Range: {range_header}")
+
+    byte1, byte2 = parse_range_header(range_header)
+    chunk, start, length, file_size = get_chunk(source_path, byte1, byte2, chunk_size)
+
+    resp = Response(chunk, 206, mimetype="video/mp4", content_type="video/mp4", direct_passthrough=True)
+    resp.headers.add("Content-Range", format_range_header(start, length, file_size))
+    logger.debug(f"chunks - Content-Range: {format_range_header(start, length, file_size)}")
+
+    return resp
